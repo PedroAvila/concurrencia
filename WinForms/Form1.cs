6c2e5603b05dd54e88a04cba32994a0566316273
Pedro Ulises Ávila Núñez <minimainor_austin@hotmail.com>
2021-12-09 14:54:28 -0500

    Flujo de tareas

--- WinForms/Form1.cs
+++ WinForms/Form1.cs
@@ -32,7 +32,7 @@ namespace WinForms
             //await Esperar();
             //var nombre = txtInput.Text;
 
-            var tarjetas = ObtenerTarjetasDeCredito(25000);
+            var tarjetas = await ObtenerTarjetasDeCredito(2500);
             var stopWatch = new Stopwatch();
             stopWatch.Start();
 
@@ -41,7 +41,6 @@ namespace WinForms
                 //var saludo = await ObtenerSaludo(nombre);
                 //MessageBox.Show(saludo);
                 await ProcesarTarjetas(tarjetas);
-
             }
             catch (HttpRequestException ex)
             {
@@ -54,32 +53,61 @@ namespace WinForms
 
         private async Task ProcesarTarjetas(List<string> tarjetas)
         {
-            var tareas = new List<Task>();
+            using var semaforo = new SemaphoreSlim(1000);
 
+            var tareas = new List<Task<HttpResponseMessage>>();
 
-            foreach (var tarjeta in tarjetas)
+            tareas = tarjetas.Select(async tarjeta =>
             {
                 var json = JsonConvert.SerializeObject(tarjeta);
                 var content = new StringContent(json, Encoding.UTF8, "application/json");
-                var respuestaTask = httpClient.PostAsync($"{apiURL}/tarjetas", content);
-                tareas.Add(respuestaTask);
+                await semaforo.WaitAsync();
+                try
+                {
+                    return await httpClient.PostAsync($"{apiURL}/tarjetas", content);
+                }
+                finally
+                {
+                    semaforo.Release();
+                }
+            }).ToList();
+
+
+            var respuestas = await Task.WhenAll(tareas);
+            var tarjetasRechazadas = new List<string>();
+
+            foreach (var respuesta in respuestas)
+            {
+                var contenido = await respuesta.Content.ReadAsStringAsync();
+                var respuestaTarjeta = JsonConvert.DeserializeObject<RespuestaTarjeta>(contenido);
+                if (!respuestaTarjeta.Aprobada)
+                {
+                    tarjetasRechazadas.Add(respuestaTarjeta.Tarjeta);
+                }
             }
 
-            await Task.WhenAll(tareas);
+            foreach (var tarjeta in tarjetasRechazadas)
+            {
+                Console.WriteLine(tarjeta);
+            }
         }
 
-        private List<string> ObtenerTarjetasDeCredito(int cantidadDeTarjetas)
+        private async Task<List<string>> ObtenerTarjetasDeCredito(int cantidadDeTarjetas)
         {
-            var tarjetas = new List<string>();
 
-            for (int i = 0; i < cantidadDeTarjetas; i++)
+            return await Task.Run(() =>
             {
-                //0000000000000001
-                //0000000000000002
-                tarjetas.Add(i.ToString().PadLeft(16, '0'));
-            }
+                var tarjetas = new List<string>();
 
-            return tarjetas;
+                for (int i = 0; i < cantidadDeTarjetas; i++)
+                {
+                    //0000000000000001
+                    //0000000000000002
+                    tarjetas.Add(i.ToString().PadLeft(16, '0'));
+                }
+
+                return tarjetas;
+            });
         }
 
 
@@ -90,7 +118,7 @@ namespace WinForms
 
         private async Task<string> ObtenerSaludo(string nombre)
         {
-            using (var respuesta = await httpClient.GetAsync($"{apiURL}/Saludos2/{nombre}"))
+            using (var respuesta = await httpClient.GetAsync($"{apiURL}/Saludos/{nombre}"))
             {
                 respuesta.EnsureSuccessStatusCode();
                 var saludo = await respuesta.Content.ReadAsStringAsync();
